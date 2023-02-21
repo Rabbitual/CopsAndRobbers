ee82a9d76be97c4d41a168d482995db9627b56d8
Escad <aeriscapella@outlook.com>
2023-02-21 04:17:33 -0800

    Change #append String parameter to Object

--- src/main/java/xyz/mauwh/message/ColoredConsoleStringBuilder.java
+++ src/main/java/xyz/mauwh/message/ColoredConsoleStringBuilder.java
@@ -9,10 +9,10 @@ import java.util.logging.Logger;
 public class ColoredConsoleStringBuilder {
 
     private static final Ansi ANSI = Ansi.ansi();
-    private static final String YELLOW = ANSI.fgYellow().boldOff().toString();
-    private static final String RED = ANSI.fgRed().boldOff().toString();
-    private static final String GREEN = ANSI.fgGreen().boldOff().toString();
-    private static final String BLUE = ANSI.fgBlue().boldOff().toString();
+    private static final Ansi YELLOW = ANSI.fgYellow().boldOff();
+    private static final Ansi RED = ANSI.fgRed().boldOff();
+    private static final Ansi GREEN = ANSI.fgGreen().boldOff();
+    private static final Ansi BLUE = ANSI.fgBlue().boldOff();
 
     private final StringBuilder builder = new StringBuilder();
 
@@ -47,8 +47,8 @@ public class ColoredConsoleStringBuilder {
     }
 
     @NotNull
-    public ColoredConsoleStringBuilder append(@NotNull String text) {
-        builder.append(text);
+    public ColoredConsoleStringBuilder append(@NotNull Object obj) {
+        builder.append(obj);
         return this;
     }
 
