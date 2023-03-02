855030cb9dd4a65c19a7ecd8644d8c4def295be4
Escad <aeriscapella@outlook.com>
2023-03-01 16:19:37 -0800

    OpenCells onCommand cleanup

--- src/main/java/xyz/mauwh/candr/command/OpenCellsCommand.java
+++ src/main/java/xyz/mauwh/candr/command/OpenCellsCommand.java
@@ -1,8 +1,5 @@
 package xyz.mauwh.candr.command;
 
-import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -26,39 +23,26 @@ public class OpenCellsCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        Audience senderAudience = messageHandler.getAudiences().sender(sender);
         if (!(sender instanceof Player)) {
-            senderAudience.sendMessage(messageHandler.getMessage(Message.PLAYERS_ONLY_COMMAND, true));
+            messageHandler.sendMessage(sender, Message.PLAYERS_ONLY_COMMAND, true);
             return true;
         }
 
         Player player = (Player)sender;
-        if (!engine.isPlayer(player)) {
-            senderAudience.sendMessage(messageHandler.getMessage(Message.IN_GAME_ONLY_COMMAND, true));
+        GameSession session = engine.getGameSession(player);
+        if (session == null) {
+            messageHandler.sendMessage(player, Message.IN_GAME_ONLY_COMMAND, true);
             return true;
         }
 
-        for (GameSession session : engine.getSessions().values()) {
-            boolean isCop = session.isCop(player);
-            boolean isRobber = session.isRobber(player);
-            boolean doorsVulnerable = session.getDoorState() == DoorState.VULNERABLE;
-            if (!isCop && !isRobber) {
-                continue;
-            }
-
-            if (isCop) {
-                Component message = messageHandler.getMessage(Message.ROBBERS_ONLY_COMMAND, true);
-                senderAudience.sendMessage(message);
-            } else if (!doorsVulnerable) {
-                Component message = messageHandler.getMessage(Message.NOT_TIME_FOR_COMMAND, true);
-                senderAudience.sendMessage(message);
-            } else {
-                session.malfunctionDoors();
-            }
-            return true;
+        if (session.isCop(player)) {
+            messageHandler.sendMessage(player, Message.ROBBERS_ONLY_COMMAND, true);
+        } else if (session.getDoorState() != DoorState.VULNERABLE) {
+            messageHandler.sendMessage(player, Message.NOT_TIME_FOR_COMMAND, true);
+        } else {
+            session.malfunctionDoors();
         }
 
-        senderAudience.sendMessage(Component.text("You have reached limbo?!", NamedTextColor.GRAY));
         return true;
     }
 
